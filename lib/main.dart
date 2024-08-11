dce21f0b7cef53f92ccf96c1a7c99767feedce64
Vipul Jha <vipuljha08@gmail.com>
2024-08-11 17:46:10 +0530

    Add build script and disable url strategy
    Signed-off-by: Vipul Jha <vipuljha08@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,9 @@
 import 'package:auth_flutter/core/app/app.dart';
 import 'package:auth_flutter/core/di/init_dependencies.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  if (kIsWeb) {
-    usePathUrlStrategy();
-  }
   await initDependencies();
   runApp(const AuthApp());
 }
